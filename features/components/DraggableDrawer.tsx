b1e2e9a9a417f57a1496951262fb27fdfba1b105
celineR95 <celiner@moveohls.com>
2022-10-30 21:32:37 +0200

    Food Cards inside drawer

--- features/components/DraggableDrawer.tsx
+++ features/components/DraggableDrawer.tsx
@@ -1,46 +1,50 @@
-import React, { useCallback, useMemo, useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useCallback, useMemo, useRef} from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
+import {FoodCard} from "./FoodCard";
+import {
+    ScrollView,
+} from 'react-native-gesture-handler';
 
-const App = () => {
-    // ref
+
+export const DraggableDrawer = () => {
     const bottomSheetRef = useRef<BottomSheet>(null);
 
-    // variables
-    const snapPoints = useMemo(() => ['20%', '50%', '90%'], []);
+    const snapPoints = useMemo(() => ['25%', '40%', '98%'], []);
 
-    // callbacks
     const handleSheetChanges = useCallback((index: number) => {
         console.log('handleSheetChanges', index);
     }, []);
 
-    // renders
-    return (
-        <View style={styles.container}>
-            <BottomSheet
-                ref={bottomSheetRef}
-                index={1}
-                snapPoints={snapPoints}
-                onChange={handleSheetChanges}
-            >
-                <View style={styles.contentContainer}>
-                    <Text>Awesome 🎉</Text>
-                </View>
-            </BottomSheet>
-        </View>
-    );
+    const count = [1, 2, 3, 4]
+
+    return <View style={styles.container}>
+        <BottomSheet
+            ref={bottomSheetRef}
+            index={1}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}
+        >
+            <ScrollView contentContainerStyle={styles.cardContainers}>
+                {count.map(() =>
+                    <FoodCard/>
+                )}
+            </ScrollView>
+        </BottomSheet>
+    </View>;
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width: 370,
-        height: 2000,
+        width: Dimensions.get("window").width,
     },
-    contentContainer: {
-        flex: 1,
-        alignItems: 'center',
+    cardContainers: {
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        display: "flex",
+        paddingBottom: 150,
     },
 });
 
-export default App;
