5aa1522982b75c485eef8b01cb959f39aad4b774
Gil Shwartz <>
2022-10-31 12:26:20 +0200

    feat: initial

--- src/features/globalStyle.ts
+++ src/features/globalStyle.ts
@@ -1,12 +1,18 @@
+import { Dimensions } from "react-native";
+
 import styled from "styled-components/native";
 
 export const BACKGROUND_COLOR = "#78b9e4";
 export const BLUE_COLOR = "#fff";
 export const NAVY_BLUE_COLOR = "#3367df";
 
+export const PURPLE = "#6938D3";
+
 export const AppContainer = styled.View`
   flex: 1;
-  background-color: pink;
+  background-color: ${PURPLE};
   align-items: center;
   justify-content: center;
+  height: ${Dimensions.get("window").height};
+  width: ${Dimensions.get("window").width};
 `;
