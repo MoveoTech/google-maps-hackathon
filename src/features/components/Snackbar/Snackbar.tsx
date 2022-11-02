bdcc2d4d10fe40be71c1acc48a1994f587a9197f
Zoya Rumin <zoyar@moveo.co.il>
2022-11-02 17:44:27 +0200

    snackbar from react native paper

---
+++ src/features/components/Snackbar/Snackbar.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { View, StyleSheet, Dimensions, Text, Image } from "react-native";
+import { Button, Snackbar } from "react-native-paper";
+import Icon from "react-native-paper/lib/typescript/components/Icon";
+
+const MyComponent = () => {
+  const [visible, setVisible] = React.useState(false);
+
+  const onToggleSnackBar = () => setVisible(!visible);
+
+  const onDismissSnackBar = () => setVisible(false);
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={onToggleSnackBar}>{visible ? "Hide" : "Show"}</Button>
+      <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: "",
+          onPress: () => {
+            // Do something
+          },
+        }}
+        style={styles.snackbar}
+      >
+        <Text style={{ color: "white", textAlign: "center" }}>
+          {/* <Image source={checkSvg} /> */}
+          You've added it to your trip
+        </Text>
+        <Image source={{ uri: checkSvg }} />
+      </Snackbar>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    width: Dimensions.get("window").width * 0.7,
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  snackbar: {
+    borderRadius: 22,
+    // display: "flex",
+    // justifyContent: "center",
+    // // width: "50%",
+  },
+});
+
+export default MyComponent;
--- src/features/pages/HomePage/HomePage.tsx
+++ src/features/pages/HomePage/HomePage.tsx
@@ -1,22 +1,24 @@
-import {Text} from "react-native";
+import { Text } from "react-native";
 import React from "react";
 
-import {LocationAutoComplete} from "../../components/LocationAutoComplete/LocationAutoComplete";
+import { LocationAutoComplete } from "../../components/LocationAutoComplete/LocationAutoComplete";
 import Map from "../../components/Map/Map";
-import {useUser} from "../../contexts/UserContext";
-import {HomepageContainer} from "./styles";
+import { useUser } from "../../contexts/UserContext";
+import { HomepageContainer } from "./styles";
+import MyComponent from "../../components/Snackbar/Snackbar";
 
 const HomePage = () => {
-    const {currentLocation, errorMsg} = useUser();
+  const { currentLocation, errorMsg } = useUser();
 
-    if (errorMsg) return <LocationAutoComplete/>;
-    if (!currentLocation) return <Text>Loading...</Text>;
+  if (errorMsg) return <LocationAutoComplete />;
+  if (!currentLocation) return <Text>Loading...</Text>;
 
-    return (
-        <HomepageContainer>
-            <Map location={currentLocation}/>
-        </HomepageContainer>
-    );
+  return (
+    <HomepageContainer>
+      <Map location={currentLocation} />
+      <MyComponent />
+    </HomepageContainer>
+  );
 };
 
 export default HomePage;
