f363405eadd5acd5fb673f439098a74d4cbb90ac
celineR95 <celiner@moveohls.com>
2022-11-03 17:18:42 +0200

    indent

--- src/features/components/Typography/Typography.tsx
+++ src/features/components/Typography/Typography.tsx
@@ -1,60 +1,58 @@
-import React, {FC} from "react";
-import {Text, TextStyle} from "react-native";
-import {ColorValue} from "react-native";
+import React, { FC } from "react";
+import { Text, TextStyle } from "react-native";
+import { ColorValue } from "react-native";
 
-import {PRIMARY} from "../../globalStyle";
+import { PRIMARY } from "../../globalStyle";
 
 export type TFontSize = "s" | "m" | "l"; // 12px, 14px, 16px
 
 const getFontSize: { [size in TFontSize]: number } = {
-    s: 12,
-    m: 14,
-    l: 16,
+  s: 12,
+  m: 14,
+  l: 16,
 };
 
 export interface ITypographyProps {
-    color?: ColorValue;
-    fontSize?: TFontSize;
-    weight?:
-        | "normal"
-        | "bold"
-        | "100"
-        | "200"
-        | "300"
-        | "400"
-        | "500"
-        | "600"
-        | "700"
-        | "800"
-        | "900";
-    children: string | string[];
-    fontFamily?:
-        string
-    style?: TextStyle;
+  color?: ColorValue;
+  fontSize?: TFontSize;
+  weight?:
+    | "normal"
+    | "bold"
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900";
+  children: string | string[];
+  fontFamily?: string;
+  style?: TextStyle;
 }
 
 const Typography: FC<ITypographyProps> = ({
-                                              color = PRIMARY,
-                                              fontSize = "m",
-                                              weight = "400",
-                                              fontFamily = "Avenir",
-                                              style,
-                                              children,
-                                          }) => {
-    
-    return (
-        <Text
-            style={{
-                fontFamily,
-                color,
-                fontSize: getFontSize[fontSize],
-                fontWeight: weight,
-                ...style,
-            }}
-        >
-            {children}
-        </Text>
-    );
+  color = PRIMARY,
+  fontSize = "m",
+  weight = "400",
+  fontFamily = "Avenir",
+  style,
+  children,
+}) => {
+  return (
+    <Text
+      style={{
+        fontFamily,
+        color,
+        fontSize: getFontSize[fontSize],
+        fontWeight: weight,
+        ...style,
+      }}
+    >
+      {children}
+    </Text>
+  );
 };
 
 export default Typography;
--- src/features/pages/HomePage/HomePageMap.tsx
+++ src/features/pages/HomePage/HomePageMap.tsx
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Map from "../../components/Map/Map";
-import {HomepageContainer} from "./styles";
-import {getNearByPlaces, IPlace} from "../../../api/googleApi";
-import {Cards} from "../../components/Card/Cards";
-import {DraggableDrawer} from "../../components/DraggableDrawer";
+import { HomepageContainer } from "./styles";
+import { getNearByPlaces, IPlace } from "../../../api/googleApi";
+import { Cards } from "../../components/Card/Cards";
+import { DraggableDrawer } from "../../components/DraggableDrawer";
 import TimelineComponent from "../../components/TimelineComponent/TimelineComponent";
 import { LocationObject } from "expo-location";
 import { LatLng } from "react-native-maps";
@@ -16,9 +16,9 @@ import { PhotosBaseURL } from "../../components/Card/InfoCard";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 
 export interface IPlaceOnMap extends IPlace {
-    marker: IMarker;
-    direction: IDirections;
-    isSelected: boolean;
+  marker: IMarker;
+  direction: IDirections;
+  isSelected: boolean;
 }
 export interface IMarker {
   id: string;
@@ -29,12 +29,12 @@ export interface IMarker {
   bgIcon?: number;
 }
 export interface IDirections {
-    id: string;
-    origin: LatLng;
-    destination: LatLng;
-    type: DirectionsType;
-    distance?: number;
-    duration?: number;
+  id: string;
+  origin: LatLng;
+  destination: LatLng;
+  type: DirectionsType;
+  distance?: number;
+  duration?: number;
 }
 
 const placesIcon = {
@@ -44,57 +44,57 @@ const placesIcon = {
 const SEARCH_RADIUS = 100;
 
 interface Props {
-    location: LocationObject;
+  location: LocationObject;
 }
 
-export const HomePageMap = ({location}: Props) => {
-    const [topFourPlaces, setTopFourPlaces] = useState<IPlaceOnMap[]>(null);
-    const [allPlaces, setAllPlaces] = useState<IPlace[]>([]);
-    const [allPlacesIndex, setAllPlacesIndex] = useState(0);
-    const [activeStep, setActiveStep] = React.useState(1);
-    const maxSteps = 4;
+export const HomePageMap = ({ location }: Props) => {
+  const [topFourPlaces, setTopFourPlaces] = useState<IPlaceOnMap[]>(null);
+  const [allPlaces, setAllPlaces] = useState<IPlace[]>([]);
+  const [allPlacesIndex, setAllPlacesIndex] = useState(0);
+  const [activeStep, setActiveStep] = React.useState(1);
+  const maxSteps = 4;
 
-    const [tripPlaces, setTripPlaces] = useState<IPlaceOnMap[]>(null);
+  const [tripPlaces, setTripPlaces] = useState<IPlaceOnMap[]>(null);
 
-    const [startingLocation, setStartingLocation] = useState<LatLng>({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
+  const [startingLocation, setStartingLocation] = useState<LatLng>({
+    latitude: location.coords.latitude,
+    longitude: location.coords.longitude,
+  });
+  const [locationType, setLocationType] =
+    useState<GoogleMapsPlaces>("restaurant");
+
+  const onSelectPlace = (place_id: string) => {
+    topFourPlaces.forEach((place) => {
+      if (place.place_id === place_id) {
+        place.isSelected = true;
+        place.direction.type = "dashed";
+      } else {
+        place.isSelected = false;
+        place.direction.type = "transparent";
+      }
     });
-    const [locationType, setLocationType] =
-        useState<GoogleMapsPlaces>("restaurant");
+    setTopFourPlaces([...topFourPlaces]);
+  };
 
-    const onSelectPlace = (place_id: string) => {
-        topFourPlaces.forEach((place) => {
-            if (place.place_id === place_id) {
-                place.isSelected = true;
-                place.direction.type = "dashed";
-            } else {
-                place.isSelected = false;
-                place.direction.type = "transparent";
-            }
-        });
-        setTopFourPlaces([...topFourPlaces]);
+  const onNextStep = () => {
+    const selectedPlace = topFourPlaces.find((place) => place.isSelected);
+    setTripPlaces((prev) => [...(prev || []), selectedPlace]);
+    const newStartingLocation: LatLng = {
+      latitude: selectedPlace.geometry.location.lat,
+      longitude: selectedPlace.geometry.location.lng,
     };
+    setStartingLocation(newStartingLocation);
+    const newLocationType: GoogleMapsPlaces = "cafe";
+    setLocationType(newLocationType);
+    //TODO: trigger indication Toast for user
+    setAllPlacesIndex(0);
+    setActiveStep(activeStep + 1);
+    calculateStep(newStartingLocation, newLocationType);
+  };
 
-    const onNextStep = () => {
-        const selectedPlace = topFourPlaces.find((place) => place.isSelected);
-        setTripPlaces((prev) => [...(prev || []), selectedPlace]);
-        const newStartingLocation: LatLng = {
-            latitude: selectedPlace.geometry.location.lat,
-            longitude: selectedPlace.geometry.location.lng,
-        };
-        setStartingLocation(newStartingLocation);
-        const newLocationType: GoogleMapsPlaces = "cafe";
-        setLocationType(newLocationType);
-        //TODO: trigger indication Toast for user
-        setAllPlacesIndex(0);
-        setActiveStep(activeStep + 1)
-        calculateStep(newStartingLocation, newLocationType);
-    };
-
-    const replaceTopFour = () => {
-        createTopPlaces();
-    };
+  const replaceTopFour = () => {
+    createTopPlaces();
+  };
 
   const createMarker = (place: IPlaceOnMap): IMarker => {
     const photoReference =
@@ -123,81 +123,85 @@ export const HomePageMap = ({location}: Props) => {
     type: "transparent",
   });
 
-    const createTopPlaces = (places?: IPlace[]) => {
-        const topFourPlaces =
-            places || allPlaces.slice(allPlacesIndex * 4, allPlacesIndex * 4 + 4);
-        if (topFourPlaces.length === 0) {
-            //TODO: alert user for zero palces left
-            console.log("no more places");
-            return;
-        }
-        (topFourPlaces as IPlaceOnMap[]).forEach((place) => {
-            place.marker = createMarker(place);
-            place.direction = createDirection(place);
-            place.isSelected = false;
-        });
-        setTopFourPlaces(topFourPlaces as IPlaceOnMap[]);
-        setAllPlacesIndex((prev) => prev + 1);
-    };
+  const createTopPlaces = (places?: IPlace[]) => {
+    const topFourPlaces =
+      places || allPlaces.slice(allPlacesIndex * 4, allPlacesIndex * 4 + 4);
+    if (topFourPlaces.length === 0) {
+      //TODO: alert user for zero palces left
+      console.log("no more places");
+      return;
+    }
+    (topFourPlaces as IPlaceOnMap[]).forEach((place) => {
+      place.marker = createMarker(place);
+      place.direction = createDirection(place);
+      place.isSelected = false;
+    });
+    setTopFourPlaces(topFourPlaces as IPlaceOnMap[]);
+    setAllPlacesIndex((prev) => prev + 1);
+  };
 
-    const calculateStep = async (
-        newStartingLocation?: LatLng,
-        newLocationType?: GoogleMapsPlaces
-    ) => {
-        try {
-            const nearbyPlacesResponse = await getNearByPlaces(
-                newStartingLocation || startingLocation,
-                SEARCH_RADIUS,
-                newLocationType || locationType
-            );
-            setAllPlaces(nearbyPlacesResponse.data.results);
-            createTopPlaces(nearbyPlacesResponse.data.results.slice(0, 4));
-        } catch (e) {
-            console.log(e);
-        }
-    };
+  const calculateStep = async (
+    newStartingLocation?: LatLng,
+    newLocationType?: GoogleMapsPlaces
+  ) => {
+    try {
+      const nearbyPlacesResponse = await getNearByPlaces(
+        newStartingLocation || startingLocation,
+        SEARCH_RADIUS,
+        newLocationType || locationType
+      );
+      setAllPlaces(nearbyPlacesResponse.data.results);
+      createTopPlaces(nearbyPlacesResponse.data.results.slice(0, 4));
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    useEffect(() => {
-        calculateStep();
-    }, []);
+  useEffect(() => {
+    calculateStep();
+  }, []);
 
-    const onDirectionsReady: (
-        place_id: string,
-        ...args: MapDirectionsResponse[]
-    ) => void = (place_id, args) => {
-        const placesIndex = topFourPlaces.findIndex(
-            (place) => place.place_id === place_id
-        );
-        topFourPlaces[placesIndex].direction.distance = args.distance;
-        topFourPlaces[placesIndex].direction.duration = args.duration;
-        topFourPlaces[
-            placesIndex
-            ].marker.tooltip = `distance: ${args.distance} , duration: ${args.duration}`;
-
-        setTopFourPlaces([...topFourPlaces]);
-    };
-
-    return (
-        <>
-            <HomepageContainer>
-                <Map
-                    location={location}
-                    topFourPlaces={topFourPlaces}
-                    onDirectionsReady={onDirectionsReady}
-                />
-            </HomepageContainer>
-            <DraggableDrawer activeStep={activeStep} maxSteps={maxSteps} setActiveStep={setActiveStep}>
-                <Cards topFourPlaces={topFourPlaces} onCardSelect={onSelectPlace}/>
-                <Button title="replace places" onPress={replaceTopFour}/>
-                <Button
-                    disabled={
-                        !Boolean(topFourPlaces?.find((place) => place.isSelected === true))
-                    }
-                    title="next"
-                    onPress={onNextStep}
-                />
-                <TimelineComponent/>
-            </DraggableDrawer>
-        </>
+  const onDirectionsReady: (
+    place_id: string,
+    ...args: MapDirectionsResponse[]
+  ) => void = (place_id, args) => {
+    const placesIndex = topFourPlaces.findIndex(
+      (place) => place.place_id === place_id
     );
+    topFourPlaces[placesIndex].direction.distance = args.distance;
+    topFourPlaces[placesIndex].direction.duration = args.duration;
+    topFourPlaces[
+      placesIndex
+    ].marker.tooltip = `distance: ${args.distance} , duration: ${args.duration}`;
+
+    setTopFourPlaces([...topFourPlaces]);
+  };
+
+  return (
+    <>
+      <HomepageContainer>
+        <Map
+          location={location}
+          topFourPlaces={topFourPlaces}
+          onDirectionsReady={onDirectionsReady}
+        />
+      </HomepageContainer>
+      <DraggableDrawer
+        activeStep={activeStep}
+        maxSteps={maxSteps}
+        setActiveStep={setActiveStep}
+      >
+        <Cards topFourPlaces={topFourPlaces} onCardSelect={onSelectPlace} />
+        <Button title="replace places" onPress={replaceTopFour} />
+        <Button
+          disabled={
+            !Boolean(topFourPlaces?.find((place) => place.isSelected === true))
+          }
+          title="next"
+          onPress={onNextStep}
+        />
+        <TimelineComponent />
+      </DraggableDrawer>
+    </>
+  );
 };
