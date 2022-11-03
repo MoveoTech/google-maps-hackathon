3793b7c347501e61421aecf9346dd2c9e633f18a
celineR95 <celiner@moveohls.com>
2022-11-03 16:33:03 +0200

    stepper WIP

--- App.tsx
+++ App.tsx
@@ -1,95 +1,95 @@
-import React, {useEffect, useState} from "react";
-import {StatusBar} from "expo-status-bar";
-import {Image, StyleSheet, Button, Text, View} from "react-native";
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Button, Text, View } from "react-native";
 import HomePage from "./src/features/pages/HomePage/HomePage";
-import {AppContainer} from "./src/features/globalStyle";
-import {UserProvider} from "./src/features/contexts/UserContext";
-import {useAuthentication} from "./src/features/hooks/useAuthentication";
-import {useLocationPermissionStatus} from "./src/features/hooks/useLocationPermissionStatus";
-import {addUser} from "./src/api/api";
-import {IUser} from "./src/features/types";
-import {useFonts} from "expo-font";
+import { AppContainer } from "./src/features/globalStyle";
+import { UserProvider } from "./src/features/contexts/UserContext";
+import { useAuthentication } from "./src/features/hooks/useAuthentication";
+import { useLocationPermissionStatus } from "./src/features/hooks/useLocationPermissionStatus";
+import { addUser } from "./src/api/api";
+import { IUser } from "./src/features/types";
+import { useFonts } from "expo-font";
 
 export default function App() {
-    const [user, setUser] = useState<IUser>();
-    const {promptAsync, request, getUserData, accessToken, userInfo} =
-        useAuthentication();
+  const [user, setUser] = useState<IUser>();
+  const { promptAsync, request, getUserData, accessToken, userInfo } =
+    useAuthentication();
 
-    const [loaded] = useFonts({
-        Avenir: require("./assets/fonts/Avenir-Heavy.ttf"),
-    });
+  const [loaded] = useFonts({
+    Avenir: require("./assets/fonts/Avenir-Heavy.ttf"),
+  });
 
-    const {locationStatus} = useLocationPermissionStatus();
+  const { locationStatus } = useLocationPermissionStatus();
 
-    const showUserInfo = () => {
-        if (userInfo) {
-            return (
-                <View style={styles.userInfo}>
-                    <Image source={{uri: userInfo.picture}} style={styles.profilePic}/>
-                    <Text>Welcome {userInfo.name}</Text>
-                    <Text>{userInfo.email}</Text>
-                </View>
-            );
-        }
-    };
-
-    const getOrAddUser = async () => {
-        const currentUser = await addUser({
-            username: userInfo.name,
-            email: userInfo.email,
-            picture: userInfo.picture,
-        });
-        setUser(currentUser);
-    };
-
-    useEffect(() => {
-        if (userInfo) getOrAddUser();
-    }, [userInfo]);
-
-    if (!loaded) {
-        return null;
+  const showUserInfo = () => {
+    if (userInfo) {
+      return (
+        <View style={styles.userInfo}>
+          <Image source={{ uri: userInfo.picture }} style={styles.profilePic} />
+          <Text>Welcome {userInfo.name}</Text>
+          <Text>{userInfo.email}</Text>
+        </View>
+      );
     }
+  };
 
-    return (
-        <UserProvider>
-            <AppContainer>
-                {locationStatus === "granted" ? (
-                    <>
-                        {user || true ? (
-                            <>
-                                {/* {showUserInfo()} */}
-                                <HomePage/>
-                            </>
-                        ) : (
-                            <Button
-                                title="Login"
-                                disabled={!request}
-                                onPress={() => promptAsync({useProxy: true})}
-                            />
-                        )}
-                    </>
-                ) : (
-                    <Text>Allow location services to use app</Text>
-                )}
-                <StatusBar style="auto"/>
-            </AppContainer>
-        </UserProvider>
-    );
+  const getOrAddUser = async () => {
+    const currentUser = await addUser({
+      username: userInfo.name,
+      email: userInfo.email,
+      picture: userInfo.picture,
+    });
+    setUser(currentUser);
+  };
+
+  useEffect(() => {
+    if (userInfo) getOrAddUser();
+  }, [userInfo]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <UserProvider>
+      <AppContainer>
+        {locationStatus === "granted" ? (
+          <>
+            {user || true ? (
+              <>
+                {/* {showUserInfo()} */}
+                <HomePage />
+              </>
+            ) : (
+              <Button
+                title="Login"
+                disabled={!request}
+                onPress={() => promptAsync({ useProxy: true })}
+              />
+            )}
+          </>
+        ) : (
+          <Text>Allow location services to use app</Text>
+        )}
+        <StatusBar style="auto" />
+      </AppContainer>
+    </UserProvider>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    userInfo: {
-        marginTop: 200,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    profilePic: {
-        width: 50,
-        height: 50,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  userInfo: {
+    marginTop: 200,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profilePic: {
+    width: 50,
+    height: 50,
+  },
 });
