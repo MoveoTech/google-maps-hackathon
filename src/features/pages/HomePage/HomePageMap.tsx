17d895a12e74c18636ba5e89c7abad0bd50f7a38
barak <barak@moveohls.com>
2022-11-06 15:20:11 +0200

    no message

--- src/features/pages/HomePage/HomePageMap.tsx
+++ src/features/pages/HomePage/HomePageMap.tsx
@@ -5,12 +5,11 @@ import { getNearByPlaces, IPlace } from "../../../api/googleApi";
 import { Cards } from "../../components/Card/Cards";
 import { DraggableDrawer } from "../../components/DraggableDrawer";
 import { LocationObject } from "expo-location";
-import { LatLng } from "react-native-maps";
+import { LatLng, Region } from "react-native-maps";
 import { MapDirectionsResponse } from "react-native-maps-directions";
 import { MarkerTypes } from "../../components/Map/components/CustomMarker";
 import { DirectionsType } from "../../components/Map/components/Directions";
 import { GoogleMapsPlaces } from "../../types";
-// import { Button } from "react-native";
 import { PhotosBaseURL } from "../../components/Card/InfoCard";
 import { GOOGLE_MAPS_APIKEY } from "@env";
 import Snackbar from "../../components/Snackbar/Snackbar";
@@ -19,11 +18,14 @@ import Button from "../../components/Button/Button";
 import RefreshIcon from "../../../../assets/refresh.png";
 import Loader from "../../components/Loader/Loader";
 import { useSnackbar } from "../../hooks/useSnackbar";
+import { Dimensions } from "react-native";
 
 export interface IPlaceOnMap extends IPlace {
   marker: IMarker;
   direction: IDirections;
   isSelected: boolean;
+  locationType: GoogleMapsPlaces;
+  timeAtPlace: number;
 }
 
 export interface IMarker {
@@ -43,7 +45,9 @@ export interface IDirections {
   distance?: number;
   duration?: number;
 }
-
+const { height, width } = Dimensions.get("window");
+const LATITUDE_DELTA = 0.03;
+const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
 const placesIcon = {
   restaurant: require(`../../../../assets/restaurant.png`),
 };
@@ -64,13 +68,27 @@ export const HomePageMap = ({ location }: Props) => {
   const [topTitle, setTopTitle] = useState("Choose an amazing breakfast");
   const [tripPlaces, setTripPlaces] = useState<IPlaceOnMap[]>([]);
   const { openSnackbar, hideSnackbar, snackbar } = useSnackbar();
+  const [showTimeline, setShowTimeline] = useState(false);
+  const [region, setRegion] = useState<Region>({
+    latitude: location?.coords?.latitude,
+    longitude: location?.coords?.longitude,
+    latitudeDelta: LATITUDE_DELTA,
+    longitudeDelta: LONGITUDE_DELTA,
+  });
+  const changeRegion = ({ lat, lng }: { lat: number; lng: number }) => {
+    setRegion({
+      latitude: lat,
+      longitude: lng,
+      latitudeDelta: LATITUDE_DELTA,
+      longitudeDelta: LONGITUDE_DELTA,
+    });
+  };
   const maxSteps = 4;
   const title = [
     "Choose an amazing breakfast",
-    "Choose and activity",
+    "Choose an activity",
     "Choose another one",
     "Choose another one",
-    `Trip to Tel-Aviv`, // TODO: Change and receive real data location from context
   ];
 
   const [startingLocation, setStartingLocation] = useState<LatLng>({
@@ -81,32 +99,48 @@ export const HomePageMap = ({ location }: Props) => {
     useState<GoogleMapsPlaces>("restaurant");
 
   const onSelectPlace = (place_id: string) => {
+    let lat, lng;
     topFourPlaces.forEach((place) => {
       if (place.place_id === place_id) {
         place.isSelected = true;
         place.direction.type = "dashed";
+        [lat, lng] = [place.geometry.location.lat, place.geometry.location.lng];
       } else {
         place.isSelected = false;
         place.direction.type = "transparent";
       }
     });
     setTopFourPlaces([...topFourPlaces]);
+    changeRegion({ lat: lat - 0.01, lng });
   };
 
-  const changeTitle = (activeStep: number) => {
-    switch (activeStep) {
-      case 1:
-        setTopTitle(title[0]);
-      case 2:
-        setTopTitle(title[1]);
-      case 3:
-        setTopTitle(title[2]);
-      case 4:
-        setTopTitle(title[3]);
-      default:
-        setTopTitle(title[4]);
+  const onNextStep = (isLastStep: boolean) => {
+    const selectedPlace = topFourPlaces.find((place) => place.isSelected);
+    setTripPlaces((prev) => [...(prev || []), selectedPlace]);
+    const newStartingLocation: LatLng = {
+      latitude: selectedPlace.geometry.location.lat,
+      longitude: selectedPlace.geometry.location.lng,
+    };
+    setStartingLocation(newStartingLocation);
+    const newLocationType: GoogleMapsPlaces = "cafe";
+    setLocationType(newLocationType);
+    openSnackbar({ title: `You've added it to your trip`, isCheckIcon: true });
+    setAllPlacesIndex(0);
+    setActiveStep((activeStep) => activeStep + 1);
+    if (isLastStep) {
+      setTopFourPlaces([]);
+      setShowTimeline(true);
+      setTopTitle("Trip summary");
+      return;
     }
+    calculateStep(newStartingLocation, newLocationType);
+    setTopTitle(title[activeStep - 1]);
   };
+
+  const replaceTopFour = () => {
+    createTopPlaces();
+  };
+
   const createMarker = (place: IPlaceOnMap): IMarker => {
     const photoReference =
       (place?.photos as any[])?.length > 0
@@ -153,6 +187,8 @@ export const HomePageMap = ({ location }: Props) => {
       place.marker = createMarker(place);
       place.direction = createDirection(place, location);
       place.isSelected = false;
+      place.locationType = locationType;
+      place.timeAtPlace = 2;
     });
     setTopFourPlaces(topFourPlaces as IPlaceOnMap[]);
     setAllPlacesIndex((prev) => prev + 1);
@@ -182,29 +218,6 @@ export const HomePageMap = ({ location }: Props) => {
     }
   };
 
-  const onNextStep = () => {
-    const selectedPlace = topFourPlaces.find((place) => place.isSelected);
-    if (selectedPlace) {
-      setTripPlaces((prev) => [...prev, selectedPlace]);
-      const newStartingLocation: LatLng = {
-        latitude: selectedPlace.geometry.location.lat,
-        longitude: selectedPlace.geometry.location.lng,
-      };
-      setStartingLocation(newStartingLocation);
-      const newLocationType: GoogleMapsPlaces = "cafe";
-      setLocationType(newLocationType);
-      //TODO: trigger indication Toast for user
-      setAllPlacesIndex(0);
-      setActiveStep((activeStep) => activeStep + 1);
-      calculateStep(newStartingLocation, newLocationType);
-      changeTitle(activeStep);
-    }
-  };
-
-  const replaceTopFour = () => {
-    createTopPlaces();
-  };
-
   useEffect(() => {
     calculateStep();
   }, []);
@@ -229,7 +242,7 @@ export const HomePageMap = ({ location }: Props) => {
     <>
       <HomepageContainer>
         <Map
-          location={location}
+          location={region}
           topFourPlaces={topFourPlaces}
           tripPlaces={tripPlaces}
           onDirectionsReady={onDirectionsReady}
@@ -241,37 +254,37 @@ export const HomePageMap = ({ location }: Props) => {
         maxSteps={maxSteps}
         setActiveStep={setActiveStep}
         topTitle={topTitle}
+        subTitle={
+          activeStep > maxSteps
+            ? "10:00 AM - 18:00 PM"
+            : `Step ${activeStep.toString()} out of ${maxSteps.toString()}`
+        }
       >
-        {activeStep > 4 ? (
-          <TimelineComponent />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <Cards topFourPlaces={topFourPlaces} onCardSelect={onSelectPlace} />
+        )}
+        {showTimeline ? (
+          <TimelineComponent tripPlaces={tripPlaces} />
         ) : (
           <>
-            {isLoading ? (
-              <Loader />
-            ) : (
-              <>
-                <Cards
-                  topFourPlaces={topFourPlaces}
-                  onCardSelect={onSelectPlace}
-                />
-                <Button
-                  title="Please offer me something else"
-                  onPress={replaceTopFour}
-                  icon={RefreshIcon}
-                  buttonType="secondary"
-                  style={{ width: "80%", margin: 8 }}
-                />
-                <Button
-                  disabled={
-                    !Boolean(
-                      topFourPlaces?.find((place) => place.isSelected === true)
-                    )
-                  }
-                  title="next"
-                  onPress={onNextStep}
-                />
-              </>
-            )}
+            <Button
+              title="Please offer me something else"
+              onPress={replaceTopFour}
+              icon={RefreshIcon}
+              buttonType="secondary"
+              style={{ width: "80%", margin: 8 }}
+            />
+            <Button
+              isDisabled={
+                !Boolean(
+                  topFourPlaces?.find((place) => place.isSelected === true)
+                )
+              }
+              title={activeStep === maxSteps ? `Let's go!` : "next"}
+              onPress={() => onNextStep(activeStep === maxSteps)}
+            />
           </>
         )}
       </DraggableDrawer>
