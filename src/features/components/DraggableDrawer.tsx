ad184b95e61318cedb6cb9ca99a140eab7d047b7
Gil Shwartz <>
2022-11-06 10:42:38 +0200

    test2

--- src/features/components/DraggableDrawer.tsx
+++ src/features/components/DraggableDrawer.tsx
@@ -1,78 +1,39 @@
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
-import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
-import { Dimensions, ScrollView, StyleSheet, View, Text } from "react-native";
-import Typography from "./Typography/Typography";
-import { Button } from "react-native-paper";
+import React, { useCallback, useMemo, useRef } from "react";
+import BottomSheet, { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import {
+  Dimensions,
+  SafeAreaView,
+  StyleSheet,
+  View,
+  // ScrollView,
+} from "react-native";
+import {
+  GestureHandlerRootView,
+  ScrollView,
+  NativeViewGestureHandler,
+} from "react-native-gesture-handler";
 
-interface Props {
-  activeStep: number;
-  children;
-  maxSteps;
-  setActiveStep: (activeStep: number) => void;
-  topTitle: string;
-}
-
-export const DraggableDrawer = ({
-  children,
-  activeStep,
-  maxSteps,
-  setActiveStep,
-  topTitle,
-}: Props) => {
+export const DraggableDrawer = ({ children }) => {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ["25%", "60%", "88%"], []);
   const handleSheetChanges = useCallback((index: number) => {
     console.log("handleSheetChanges", index);
   }, []);
-  const data = useMemo(
-    () =>
-      Array(50)
-        .fill(0)
-        .map((_, index) => `index-${index}`),
-    []
-  );
-
-  const renderItem = useCallback(
-    ({ item }) => (
-      <View>
-        <Text>{item}</Text>
-      </View>
-    ),
-    []
-  );
 
   return (
     <BottomSheet
       ref={bottomSheetRef}
-      index={1}
+      index={0}
       snapPoints={snapPoints}
       onChange={handleSheetChanges}
     >
-      <BottomSheetFlatList
-        data={data}
-        keyExtractor={(i) => i}
-        renderItem={renderItem}
-      >
-        <View style={styles.topViewWrapper}>
-          <Button
-            icon={require("../../../assets/pin.png")}
-            onPress={() => setActiveStep(activeStep - 1)}
-            children={""}
-          />
-        </View>
-        <View style={{ alignItems: "center" }}>
-          <Typography style={{ fontWeight: "500" }}>{topTitle}</Typography>
-          <Typography>
-            Step {activeStep.toString()} out of {maxSteps.toString()}
-          </Typography>
-        </View>
-        <View style={styles.bottomContainer} />
-        <View style={styles.container}>
-          <ScrollView contentContainerStyle={styles.cardContainers}>
-            {children}
-          </ScrollView>
-        </View>
-      </BottomSheetFlatList>
+      {/* <View style={styles.container}> */}
+      <NativeViewGestureHandler disallowInterruption>
+        <ScrollView contentContainerStyle={styles.cardContainers}>
+          {children}
+        </ScrollView>
+      </NativeViewGestureHandler>
+      {/* </View> */}
     </BottomSheet>
   );
 };
@@ -87,15 +48,6 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     display: "flex",
-    marginTop: 20,
-  },
-  bottomContainer: {
-    marginTop: 15,
-    borderBottomColor: "lightgrey",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  topViewWrapper: {
-    position: "absolute",
-    zIndex: 200,
+    marginTop: 30,
   },
 });
