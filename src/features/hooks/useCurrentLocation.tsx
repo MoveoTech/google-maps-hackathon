3fefdc392915b264a152bd406ee5d22c526b9f31
barak <barak@moveohls.com>
2022-11-10 18:05:15 +0200

    no message

--- src/features/hooks/useCurrentLocation.tsx
+++ src/features/hooks/useCurrentLocation.tsx
@@ -1,33 +1,33 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
 const mockLocation = {
-    coords: {
-        accuracy: 35,
-        altitude: 27.983800888061523,
-        altitudeAccuracy: 1.3736238479614258,
-        heading: -1,
-        latitude: 32.06388108937858,
-        longitude: 34.7729407013912,
-        speed: -1,
-    },
-    timestamp: 1666617989710.852,
+  coords: {
+    accuracy: 35,
+    altitude: 27.983800888061523,
+    altitudeAccuracy: 1.3736238479614258,
+    heading: -1,
+    latitude: 32.06388108937858,
+    longitude: 34.7729407013912,
+    speed: -1,
+  },
+  timestamp: 1666617989710.852,
 };
 
 export const useCurrentLocation = () => {
-    const [location, setLocation] = useState<Location.LocationObject>();
-    const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState<Location.LocationObject>();
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                let currentLocation = mockLocation// await Location.getCurrentPositionAsync({});
-                setLocation(currentLocation);
-            } catch (e) {
-                setErrorMsg(e);
-            }
-        })();
-    }, []);
+  useEffect(() => {
+    (async () => {
+      try {
+        let currentLocation = await Location.getCurrentPositionAsync({});
+        setLocation(currentLocation);
+      } catch (e) {
+        setErrorMsg(e);
+      }
+    })();
+  }, []);
 
-    return {location, errorMsg};
+  return { location, errorMsg };
 };
